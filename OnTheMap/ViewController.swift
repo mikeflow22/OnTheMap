48694577eee42e198f62ee2a87049c787616a6af
Michael R.F <mrflowers22@uwalumni.com>
2020-02-22 08:14:18 -0800

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Michael Flowers on 2/22/20.
+//  Copyright © 2020 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
