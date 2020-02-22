2e89cf0f12b82c015e4b5944d3c73c7b91f102c1
Michael R.F <mrflowers22@uwalumni.com>
2020-02-22 08:15:33 -0800

    created a struct that represents the top level dictionary in the json.

---
+++ OnTheMap/Models/TopLevelDictionary.swift
@@ -0,0 +1,13 @@
+//
+//  TopLevelDictionary.swift
+//  OnTheMap
+//
+//  Created by Michael Flowers on 2/22/20.
+//  Copyright © 2020 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+
+struct TopLevelDictionary: Codable {
+    let results: [Student]
+}
