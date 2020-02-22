965dd370ee68169efc8dade3155f43c35aed5c64
Michael R.F <mrflowers22@uwalumni.com>
2020-02-22 09:55:47 -0800

    constructed the getAllStudents  function and returned the ones with full names.

--- OnTheMap/StudentAPIs/StudentAPIs.swift
+++ OnTheMap/StudentAPIs/StudentAPIs.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class StudnetAPIs {
+class StudentAPIs {
     
     //MARK: - Endpoints
     enum Endpoints {
@@ -16,11 +16,13 @@ class StudnetAPIs {
         static let limitQuery = "?limit="
         static let uniqueKeyQuery = "?uniqueKey="
         
+        case getAllStudents
         case limitStudentSearch(Int)
         case searchWithUniqueKey(String)
         
         var stringValue: String {
             switch self {
+            case .getAllStudents: return Endpoints.base
             case .limitStudentSearch(let number): return Endpoints.base + Endpoints.limitQuery + "\(number)"
             case .searchWithUniqueKey(let userId):  return  Endpoints.base + Endpoints.uniqueKeyQuery + "\(userId)"
             }
@@ -29,13 +31,53 @@ class StudnetAPIs {
         var url: URL {
             return URL(string: stringValue)!
         }
-        
-        
-        
-        
-        
     }
     
-    
+    class func getAllStudents(completion: @escaping ([Student]?, Error?) -> Void){
+        let task = URLSession.shared.dataTask(with: Endpoints.getAllStudents.url) { (data, response, error) in
+            if let response = response as? HTTPURLResponse {
+                print("Response for \(#function): \(response.statusCode)")
+            }
+            
+            if let error = error {
+                print("Error in file: \(#file) in the body of the function: \(#function)\n on line: \(#line)\n Readable Error: \(error.localizedDescription)\n Technical Error: \(error)\n")
+                DispatchQueue.main.async {
+                    completion([], error)
+                }
+                return
+            }
+            
+            guard let data = data else {
+                print("Error in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
+                DispatchQueue.main.async {
+                    completion([], error)
+                }
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            var studentsWithFullNames = [Student]()
+            
+            do {
+                let returnedStudents = try decoder.decode(TopLevelDictionary.self, from: data).results
+                for student in returnedStudents {
+                    if student.firstName != "" && student.lastName != "" {
+//                        print("Student's name: \(student.fullName)")
+                        studentsWithFullNames.append(student)
+                    }
+                }
+                DispatchQueue.main.async {
+                    completion(studentsWithFullNames, nil)
+                }
+            } catch  {
+                print("Error in: \(#function)\n Readable Error: \(error.localizedDescription)\n Technical Error: \(error)")
+                DispatchQueue.main.async {
+                    completion([], error)
+                }
+            }
+        }
+        task.resume()
+    }
     
 }
