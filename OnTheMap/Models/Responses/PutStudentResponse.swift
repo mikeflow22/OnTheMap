786f78b6e735ff10abd2e388a68881f250e0f59a
Michael R.F <mrflowers22@uwalumni.com>
2020-02-24 13:33:23 -0800

    added PutStudentResponse struct to project navigator

---
+++ OnTheMap/Models/Responses/PutStudentResponse.swift
@@ -0,0 +1,13 @@
+//
+//  PutStudentResponse.swift
+//  OnTheMap
+//
+//  Created by Michael Flowers on 2/24/20.
+//  Copyright © 2020 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+
+struct PutStudentResponse: Codable {
+    let updatedAt: String
+}
