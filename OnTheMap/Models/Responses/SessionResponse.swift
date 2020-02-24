86f0d1db05525af058c982687547ec29fa413bb1
Michael R.F <mrflowers22@uwalumni.com>
2020-02-24 13:37:22 -0800

    added SessionResponse struct which has two nested structs inside of itself.

---
+++ OnTheMap/Models/Responses/SessionResponse.swift
@@ -0,0 +1,24 @@
+//
+//  SessionResponse.swift
+//  OnTheMap
+//
+//  Created by Michael Flowers on 2/24/20.
+//  Copyright © 2020 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+
+struct SessionResponse: Codable {
+    let account: [AccountInformation]
+    let session: [SessionInformation]
+
+struct AccountInformation: Codable {
+    let registered: Bool
+    let key: String
+}
+
+struct SessionInformation: Codable {
+    let id: String
+    let expeiration: String
+    }
+}
