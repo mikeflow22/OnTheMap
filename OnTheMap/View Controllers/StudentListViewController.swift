47a4419645b90f67d869ad464d41d2882f2ce37e
Michael R.F <mrflowers22@uwalumni.com>
2020-02-26 17:05:10 -0800

    clean up

--- OnTheMap/View Controllers/StudentListViewController.swift
+++ OnTheMap/View Controllers/StudentListViewController.swift
@@ -9,16 +9,16 @@
 import UIKit
 
 class StudentListViewController: UIViewController {
-
+    
     var students: [Student]? {
-          didSet {
-              guard let students = students else {
-                  print("Error in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
-                  return
-              }
+        didSet {
+            guard let students = students else {
+                print("Error in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
+                return
+            }
             self.tableView.reloadData()
-          }
-      }
+        }
+    }
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
@@ -29,41 +29,47 @@ class StudentListViewController: UIViewController {
     }
     
     func get100Students(){
-           StudentAPIs.getStudentsWithALimit(studentLimit: 100) { (students, error) in
-               if let error = error {
-                   print("Error in file: \(#file) in the body of the function: \(#function)\n on line: \(#line)\n Readable Error: \(error.localizedDescription)\n Technical Error: \(error)\n")
-                   return
-               }
-               
-               guard let returnedStudents = students  else {
-                   print("Error in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
-                   return
-               }
-               DispatchQueue.main.async {
-                   self.students = returnedStudents
-               }
-           }
-       }
+        StudentAPIs.getStudentsWithALimit(studentLimit: 100) { (students, error) in
+            if let error = error {
+                print("Error in file: \(#file) in the body of the function: \(#function)\n on line: \(#line)\n Readable Error: \(error.localizedDescription)\n Technical Error: \(error)\n")
+                return
+            }
+            
+            guard let returnedStudents = students  else {
+                print("Error in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
+                return
+            }
+            DispatchQueue.main.async {
+                self.students = returnedStudents
+            }
+        }
+    }
     
     @IBAction func logout(_ sender: UIBarButtonItem) {
+        StudentAPIs.logout(completion: self.handleLogoutResponse(success:error:))
     }
     
     @IBAction func refresh(_ sender: UIBarButtonItem) {
+        //clear student array
+        self.students?.removeAll()
+        
+        //network call to retrieve students
+        get100Students()
     }
     
     @IBAction func addStudentLocation(_ sender: UIBarButtonItem) {
     }
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
 
 extension StudentListViewController: UITableViewDelegate,  UITableViewDataSource {
