f441abe8ae1476616c4536e0237f735af9630c04
Michael R.F <mrflowers22@uwalumni.com>
2020-02-27 08:30:55 -0800

    added scene for adding location and added another scene to show the detail of said location.
    added applicable source files.

---
+++ OnTheMap/View Controllers/DetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DetailViewController.swift
+//  OnTheMap
+//
+//  Created by Michael Flowers on 2/27/20.
+//  Copyright © 2020 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func finishButtonTapped(_ sender: UIButton) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
+
+extension DetailViewController: MKMapViewDelegate {
+    
+}
